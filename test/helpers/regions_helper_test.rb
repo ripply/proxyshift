7efbd51febfc574484ae02b3e35684b46dd58c84
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 16:11:31 -0600

    Add basic Region model tests

--- test/helpers/regions_helper_test.rb
+++ test/helpers/regions_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class RegionsHelperTest < ActionView::TestCase
+  test 'should not save region without data' do
+    region = Region.new
+    assert_not region.save
+  end
+
+  test 'should save region given a name' do
+    region = Region.new name: 'test_region'
+    assert region.save
+  end
 end
