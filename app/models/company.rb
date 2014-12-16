4ed4c03f177b5460908a2cde4e8ba5c77aa2dafe
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 17:28:22 -0600

    Add website validation to Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :website, format: {:with => /\A\z|\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/}
 
   has_many :regions
 end
