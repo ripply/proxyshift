0ccbe9754cfadf0e2f051e586c8eb7ece0f95201
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 16:00:36 -0600

    Simple User model validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :account, presence: true
   validates :email, presence: true
+  # http://apidock.com/rails/ActiveModel/Validations/ClassMethods/validates
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, presence: true
 
   has_one :type
--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -1,4 +1,35 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+  test 'should not save without data' do
+    user = User.new
+    assert_not user.save, 'Saved without data'
+  end
+
+  test 'should not save with just account' do
+    user = User.new account: 'test_account'
+    assert_not user.save
+  end
+
+  test 'should not save with just email' do
+    user = User.new email: 'example@example.org'
+    assert_not user.save
+  end
+
+  test 'should not save with just password' do
+    user = User.new password: 'secret'
+    assert_not user.save
+  end
+
+  test 'should save if account, email, password specified and valid' do
+    user = User.new account: 'test_account', email: 'example@example.org', password: 'secret'
+    assert user.save
+  end
+
+  test 'should fail if invalid email given' do
+    ['example', 'example@asdf', '12341234', '12341234@microsoft'].each do |invalid_email|
+      user = User.new account: 'test_account', email: invalid_email, password: 'secret'
+      assert_not user.save
+    end
+  end
 end
