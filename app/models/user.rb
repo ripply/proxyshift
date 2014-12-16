a09fd029ed1cf7f72691533c5d6439ddb437df9b
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 16:30:21 -0600

    Add TODO for setting up secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   # http://apidock.com/rails/ActiveModel/Validations/ClassMethods/validates
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  # TODO: http://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
   validates :password, presence: true
 
   has_one :type
