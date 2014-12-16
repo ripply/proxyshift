61db273e66f755e658512863fe80458627bae006
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 16:11:21 -0600

    Add basic Company model tests

--- test/helpers/company_helper_test.rb
+++ test/helpers/company_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class CompanyHelperTest < ActionView::TestCase
+  test 'should not save company without data' do
+    company = Company.new
+    assert_not company.save, 'Saved without data'
+  end
+
+  test 'should save given a company name' do
+    company = Company.new name: 'test_company'
+    assert company.save, 'Failed to save company given a name'
+  end
 end
