62e958b38f099a711087e7ab2a34817c6c975cf4
Chase Ripplinger <chase.ripplinger@gmail.com>
2014-12-16 20:04:57 -0600

    Model: Company => Companies

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -3,12 +3,12 @@ require_relative '../../app/controllers/companies_controller'
 
 class CompaniesControllerTest < ActionController::TestCase
   def setup
-    @controller = CompanyController.new
+    @controller = CompaniesController.new
   end
 
   test 'should not create company' do
     assert_no_difference 'Company.count' do
-      post :create, post: {}
+      post :create, post: {company: 'test_company'}
     end
 
     assert_redirected_to post_path(assigns(:post))
