c97bf42cf7808b214e2efc5e680d974aa2991479
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-01-12 21:17:48 -0600

    Modify named route if name is already in use

--- app/models/dynamic_router.rb
+++ app/models/dynamic_router.rb
@@ -10,15 +10,36 @@ class DynamicRouter
   # @param ActiveRecord::Base parent
   # @param ActiveRecord::Base category
   #
+  # @returns Array<String>
+  #
 
   def self.recurse(results, base_uri, parent, category)
     child_base_uri = "#{base_uri}/#{category.name}"
-    puts "Routing #{child_base_uri}"
-    results << "get '#{child_base_uri}', :to => 'categories#show', defaults: { id: #{category.id} }"
+    route = (DynamicRouter.named_route_for_uri child_base_uri).to_sym
+    # if route turns out to be calculated to an existing route then we need to change it or rails will error due to duplicate named routes
+    exists = false
+    begin
+      exists = false
+      Rails.application.routes.named_routes.each do |existing_route|
+        if existing_route == route
+          exists = true
+          break
+        end
+      end
+      route = "#{route.to_s}_".to_sym
+    end while exists
+
+    results << "get '#{child_base_uri}', :to => 'categories#show', as: '#{route}', defaults: { id: #{category.id}, route: '#{route}'}"
     category.children.each do |child|
       DynamicRouter.recurse results, child_base_uri, category, child
     end
+    
     results
+  end
+
+  def self.named_route_for_uri(uri)
+    # strip leading and ending garbage underscores or forward slashes
+    ((uri.sub /^[_\/]*/, '').sub! /[_\/]*$/, '').downcase.gsub '/', '_' unless uri.nil?
   end
 
   def self.load
@@ -27,7 +48,11 @@ class DynamicRouter
       Category.where(:parent_id => nil).each do |root|
         DynamicRouter.recurse routes_strings, '/', nil, root
       end
-      instance_eval(routes_strings.join("\n"), 'routes', 0)
+      begin
+        instance_eval(routes_strings.join("\n"), 'dynamic routes', 0)
+      rescue Exception => e
+        raise e, "#{e.message}\nRoutes:::: #{routes_strings.join "\n"}", e.backtrace
+      end
     end
   end
 
