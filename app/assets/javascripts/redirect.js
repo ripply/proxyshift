730ddaf1218f9b374831b555c392fdfa2040be94
M Barakat <mbbarakat@gmail.com>
2015-01-16 05:58:02 +0100

    Added redirect.js to app/assets/js & to config/initializers/assets.rb.

---
+++ app/assets/javascripts/redirect.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Zang on 2015-01-16.
+ */
+var targetURL = "../../";
+var countdownfrom = 6;
+
+function countRedirect(){
+    if (countdownfrom!=1){
+        countdownfrom-=1;
+        document.getElementById('redirect').innerHTML = countdownfrom;
+    }
+    else{
+        window.location=targetURL;
+        return;
+    }
+    setTimeout("countRedirect()",1000);
+}
+
+countRedirect();
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( redirect.js )
 
 # Precompile additional assets.
 # application.js, application.css.sass, and all non-JS/CSS in app/assets folder are already added.
