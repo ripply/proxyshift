711a1fa484a6143f35636f9364995fc5814d416b
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-01-20 07:59:42 -0600

    Add support for expiring of sessions after 24hours
    This does not correctly handle DST boundaries so needs to be eventually switched over to ActiveSupport::TimeWithZone

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,11 @@
+require 'active_support/core_ext'
+
 module SessionsHelper
 
   def sign_in(user)
     #cookies.permanent.signed[:remember_token] = [user.id]
     session[:user_id] = user.id
+    update_session_expiration_time
     self.current_user = user
   end
 
@@ -15,11 +18,17 @@ module SessionsHelper
   end
 
   def signed_in?
-    !current_user.nil?
+    !current_user.nil? && !signin_expired?
+  end
+
+  def signin_expired?
+    session[:session_expires].nil? ||
+        ((time_now > session[:session_expires]) && update_session_expiration_time)
   end
 
   def sign_out
     session.delete :user_id
+    session.delete :session_expires
     self.current_user = nil
   end
 
@@ -42,6 +51,15 @@ module SessionsHelper
   end
 
   private
+
+  def time_now
+    Time.now.utc
+  end
+
+  def update_session_expiration_time
+    # TODO: ActiveSupport::TimeWithZone, because this does not handle DST boundaries correctly
+    session[:session_expires] = time_now + 1.days
+  end
 
   def user_from_session
     User.find_by_id user_id
