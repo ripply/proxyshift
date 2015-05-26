e0790ff33ea60424195f5369ca8db52696687b15
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-05-26 08:52:45 -0500

    Add a basic signup controller/template, doesn't work yet

---
+++ ionic/www/js/controllers/SignupController.js
@@ -0,0 +1,57 @@
+/**
+ * SignupController
+ */
+angular.module('scheduling-app.controllers')
+    .controller('SignupController', [
+        '$scope',
+        '$http',
+        '$state',
+        function($scope, $http, $state) {
+
+            $scope.user = {
+                name: null,
+                username: null,
+                password: null,
+                squestion: null,
+                sanswer: null
+            };
+
+            $scope.signup = function() {
+                // TODO
+            };
+
+            $scope.$on('event:signup-required', function(e, rejection) {
+                // clear any error messages
+                $scope.message = null;
+                // reset existing midtyped username/password
+                $scope.user.name = null;
+                $scope.user.username = null;
+                $scope.user.password = null;
+                $scope.user.squestion = null;
+                $scope.user.sanswer = null;
+
+                $scope.signupModal.show();
+            });
+
+            $scope.$on('event:signup-successful', function() {
+                $scope.message = null;
+                $scope.user.name = null;
+                $scope.user.username = null;
+                $scope.user.password = null;
+                $scope.user.squestion = null;
+                $scope.user.sanswer = null;
+
+                $scope.signupModal.hide();
+            });
+
+            $scope.$on('event:signup-failed', function(e, message) {
+                $scope.message = message;
+            });
+
+            $scope.$on('event:signup-complete', function() {
+                $state.go('app.home', {}, {reload: true, inherit: false});
+            });
+
+        }]
+)
+;
