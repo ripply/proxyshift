c09ec2cb7ccf65c266c51e23081745feb470548a
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-05-29 16:26:33 -0500

    Demo: Fetch users from server and display in playlists

--- ionic/www/js/controllers/Controllers.js
+++ ionic/www/js/controllers/Controllers.js
@@ -1,22 +1,36 @@
 angular.module('scheduling-app.controllers', [
-    'scheduling-app.models'
+  'scheduling-app.models'
 ])
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-  $scope.add = function() {
-    $scope.playlists.push({
-      title: 'test!?', id: $scope.playlists.length
-    });
-  };
-})
+    .controller('PlaylistsCtrl', [
+      '$scope',
+      'UsersModel',
+      function($scope, UsersModel) {
+        $scope.playlists = [
+          { title: 'Reggae', id: 1 },
+          { title: 'Chill', id: 2 },
+          { title: 'Dubstep', id: 3 },
+          { title: 'Indie', id: 4 },
+          { title: 'Rap', id: 5 },
+          { title: 'Cowbell', id: 6 }
+        ];
+        $scope.add = function() {
+          $scope.playlists.push({
+            title: 'test!?', id: $scope.playlists.length
+          });
+        };
+        $scope.fetch = function() {
+          UsersModel.getList().then(function(users) {
+            console.log("Successfully fetched users!");
+            $scope.playlists = users;
+          }, function(err) {
+            $scope.playlists = [
+              {username: 'Failed to fetch users'}
+            ];
+          });
+        };
+        $scope.fetch();
+}])
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
