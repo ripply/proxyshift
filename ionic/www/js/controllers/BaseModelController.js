9ba1db1d9d2ee7511bb2500ea68dc2bf2daf47f8
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-06-17 07:45:50 -0500

    WIP of base model controller on train
    Basic idea behind controller:
    https://stackoverflow.com/questions/21483555/angular-js-best-practice-extending-controllers-overriding-controller-defaults

---
+++ ionic/www/js/controllers/BaseModelController.js
@@ -0,0 +1,39 @@
+angular.module('scheduling-app.controllers')
+    .controller('BaseModelController', [
+        '$scope',
+        '$injector',
+        function($scope,
+                 $injector) {
+            $scope.init = function(model, baseObject, addFunction) {
+                $scope._modelName = model;
+                $scope._baseObject = baseObject
+                // this will throw if the service doesnt exist
+                $scope._model = $injector.get(model);
+                $scope._addFunction = addFunction;
+            };
+
+            $scope.revertToDefaultObject = function() {
+                $scope.object = angular.extend($scope._baseObject);
+                return $scope.object;
+            };
+
+            $scope.fetch = function() {
+                $scope._model.getList().then(function(objects) {
+                    $scope.objects = objects;
+                }, function(err) {
+                    $scope.objects = null;
+                    $scope.error = err;
+                });
+            };
+
+            $scope.add = function() {
+                $scope.object = $scope._addFunction($scope.revertToDefaultObject());
+                $scope._model.post($scope.object)
+                    .then(function() {
+                        //success
+                    }, function() {
+                        //error
+                    });
+            };
+        }
+    ]);
