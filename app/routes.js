01ed32bd434038ce97cb8f553bf1193dd5a09d98
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-07-11 22:50:09 -0500

    Remove erroneous includes from /app/routes.js
    Eventually this file will need to be pruned, it has no content at all

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,3 @@
-var home = require('../controllers/home'),
-    shifts = require('../controllers/shifts'),
-    users = require('../controllers/users'),
-    groups = require('../controllers/groups'),
-    categories = require('../controllers/categories'),
-    _ = require('underscore'),
-    utils = require('./utils'),
-    models = require('./models');
-
 module.exports.initialize = function(app) {
     return require('../routes')(app, {
         auth: true
