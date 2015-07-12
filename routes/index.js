943becbce78823f7f12ca795af4bfdb773bb5864
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-07-11 21:26:30 -0500

    Delete categories as it is not in line with database anymore

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ module.exports = function(app, settings){
     require('./preauth')(app, settings);
     require('./misc/auth')(app, settings);
     require('./shifts')(app, settings);
-    require('./categories')(app, settings);
     require('./users')(app, settings);
     require('./groups')(app, settings);
 };
