46426c505fc27afb3de8a852d2642a5cd207d9ff
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-07-13 08:11:33 -0500

    More API definition

--- routes/groups.js
+++ routes/groups.js
@@ -11,27 +11,36 @@ module.exports = function(app, settings){
     groupsRouter.get('/:id', groups.getById);
     // GET /classes - get all groupuserclasses for this group
     // GET /:id/permissions - get your group permissins
-    // GET /:id/permissions/:id - get a users group permissions
     // GET /:id/users - get all group members
+    // GET /:id/users/:id/permissions - get a users group permissions
+    // GET /:id/class - get all groupuserclasses
+    // GET /:id/class/:id - get a specific groupuserclass
+    // GET /:id/areas - get all areas attached
+    // GET /:id/locations - get all locations attached
     // POST / - create a group
     // POST /:id/permissions - create a group permission set
     groupsRouter.post('/', groups.add);
-    // POST /:id/area - create an area
+    // POST /:id/areas - create an area
     // POST /:id/locations - create a location
-    // POST /:id/user/:id - add a user to a group
+    // POST /:id/users/:id/permissions/:id - add a user to a group
+    // POST /:id/classes - create a groupuserclass
 
     // PUT / - same as POST
     groupsRouter.put('/', groups.add);
     // PUT /:id/location - same as POST
 
-    // PATCH /:id - update a group
+    // PATCH /:id - update a group and group settings
     groupsRouter.patch('/:id', groups.update);
     // PATCH /:id/permissions/:id - update a group permission set
+    // PATCH /:id/users/:id/permissions/:id - update a users permission set
+    // PATCH /:id/classes/:id - update a groupuserclass
     // DELETE /:id - delete a group
     groupsRouter.delete('/:id', groups.delete);
     // DELETE /:id/permissions/:id - delete a group permission set
     // DELETE /:id/users/:id - remove a user from a group
-    // DELETE /:id/area/:id - remove an area from this group
+    // DELETE /:id/areas/:id - remove an area from this group
+    // DELETE /:id/locations/:id - remove a location from a group
+    // DELETE /:id/classes/:id - remove a groupuserclass
 
     app.use(groups.route, groupsRouter);
 
--- routes/locations.js
+++ routes/locations.js
@@ -11,11 +11,13 @@ module.exports = function(app, settings){
     // GET /new - get new shifts
     // GET /open - get open shifts
     // GET /:id/permissions - get your permissions for this location
-    // GET /:id/permissions/:id - get permissions for user
+    // GET /:id/permissions/:id/ - get permissions for user
     // POST /shift - create a shift for this location
+    // POST /:id/permissions/:id/users/:id - add user to location with permissions: also will update a users permission if they already have one set for this location
     // PATCH /:id - update a location
     // PATCH /:id/permissions/:id - update a users permissions
     // DELETE /:id - delete a location
+    // DELETE /:id/users/:id - remove a user from a location
 
     app.use(locations.route, locationsRouter);
 
