2d5c47e6dd875c623f43e2917c657e577b427723
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-07-14 21:33:08 -0500

    Things almost work, but new auth stuff isnt being called correctly

---
+++ controllers/permissions.js
@@ -0,0 +1,42 @@
+var groupOwnerOrGroupMember = ['group owner', 'group member'];
+
+module.exports = {
+
+    auth: {
+
+        groupOwnerOrGroupMemeber: function (req, act) { // owner/member
+            // check if the user has access to this group
+            // the user will be a part of the group
+            // or own the group
+            // select groups.* from groups inner join usergroups
+            //   on groups.id = usergroups.group_id and usergroups.user_id = 3
+            //   union select * from groups where user_id = 3;
+            var group_id = req.params.id;
+            var user_id = req.user.id;
+            return models.Group.query(function (q) {
+                q.select('groups.*').innerJoin('usergroups', function () {
+                    this.on('groups.id', '=', 'usergroups.group_id')
+                        .andOn('usergroups.user_id', '=', user_id);
+                })
+                    .where('groups.id', '=', group_id)
+                    .union(function () {
+                        this.select('groups.*')
+                            .from('groups')
+                            .where('user_id', '=', user_id)
+                            .andWhere('id', '=', group_id);
+                    });
+            })
+                .fetch({require: true})
+                .then(function (group) {
+                    console.log("SUP YEA COMEON IN");
+                    return true;
+                })
+                .catch(function (err) {
+                    console.log("HEYY!");
+                    return false;
+                });
+        }
+
+    }
+
+};
--- routes/misc/roles.js
+++ routes/misc/roles.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 
 var controllers = [
+    require('../../controllers/permissions'),
     require('../../controllers/groups'),
     require('../../controllers/shifts'),
     require('../../controllers/users')
@@ -25,99 +26,126 @@ module.exports = function(app, roles) {
     app.use(roles.middleware());
 
     _.each(controllers, function(controller) {
-        if (controller.hasOwnProperty('route')) {
+        if (controller.hasOwnProperty('route') ||
+            controller.hasOwnProperty('auth')) {
             var route = controller.route;
-            // make sure that route does not have an ending /
-            if (route.indexOf('/', route.length - '/'.length) !== -1) {
-                console.log("WARNING: Route (" + route + ") ends with / auth roles may get messed up");
+            if (route !== undefined) {
+                // make sure that route does not have an ending /
+                if (route.indexOf('/', route.length - '/'.length) !== -1) {
+                    console.log("WARNING: Route (" + route + ") ends with / auth roles may get messed up");
+                }
             }
 
             _.each(_.keys(controller), function(possibleRoute) {
+                console.log("!?!?!?!:" + possibleRoute);
                 // check if key could be a route
                 // a route will be an object
                 var possibleRouteValue = controller[possibleRoute];
                 if (possibleRouteValue instanceof Object) {
-                    _.each(_.keys(possibleRouteValue), function(verb) {
-                        verb = verb.toLowerCase();
+                    if (possibleRoute == 'auth') {
+                        _.each(_.keys(possibleRouteValue), function (actionName) {
+                            var authFunction = possibleRouteValue[actionName];
+                            if (typeof authFunction == "function") {
+                                if (actionName instanceof Array) {
+                                    // if the action name is an array of multiple strings
+                                    // then turn it into a single one!
+                                    actionName.sort();
+                                    actionName = flattenArray(actionName).toLowerCase();
+                                }
+                                console.log(actionName + " => " + authFunction);
+                                app.use(actionName, authFunction);
+                            }
+                        });
+                    } else {
+                        _.each(_.keys(possibleRouteValue), function (verb) {
+                            verb = verb.toLowerCase();
 
-                        var authAndRouteObject = possibleRouteValue[verb];
-                        if (authAndRouteObject instanceof Object && verbs.indexOf(verb) >= 0) {
-                            // verb!!
-                            // check if they have an auth property
-                            var auth = authAndRouteObject.auth;
-                            var subRoute = authAndRouteObject.route;
+                            var authAndRouteObject = possibleRouteValue[verb];
+                            if (authAndRouteObject instanceof Object) {
+                                if (verbs.indexOf(verb) >= 0) {
+                                    // verb!!
+                                    // check if they have an auth property
+                                    var auth = authAndRouteObject.auth;
+                                    var subRoute = authAndRouteObject.route;
 
-                            var fullRoute = route + possibleRoute;
+                                    var fullRoute = route + possibleRoute;
 
-                            if (subRoute !== undefined &&
-                                typeof subRoute == 'function') {
-                                if (auth !== undefined) {
-                                    // create action name for auth
-                                    // action name will essentially just be the verb + route
-                                    var action = verb + " " + fullRoute;
+                                    if (subRoute !== undefined &&
+                                        typeof subRoute == 'function') {
+                                        if (auth !== undefined) {
+                                            // create action name for auth
+                                            // action name will essentially just be the verb + route
+                                            var action = verb + " " + fullRoute;
 
-                                    var fullRoleActionText = action
-                                        .replace(":", ";"); // due to bug somewhere, if there is a colon in the action name
-                                                            // it will be converted to the path param which means
-                                                            // the action will never execute since the action is different
-                                                            // this means that you can have it setup for very specific routes though
+                                            var fullRoleActionText = action
+                                                .replace(":", ";"); // due to bug somewhere, if there is a colon in the action name
+                                                                    // it will be converted to the path param which means
+                                                                    // the action will never execute since the action is different
+                                                                    // this means that you can have it setup for very specific routes though
 
-                                    if (typeof auth == 'function') {
-                                        // auth is a function which means
-                                        // we don't have to do anything special
-                                        roles.use(fullRoleActionText, fullRoute, auth);
-                                        app[verb](fullRoute, user.can(fullRoleActionText));
-                                    } else if (typeof auth == 'string') {
-                                        // this means that we should use this string
-                                        // in place of the fullRoleActionText we
-                                        // are creating.
-                                        // the purpose of this is to allow reuse of
-                                        // auth code and to allow simple naming of
-                                        // requirements
-                                        app[verb](fullRoute, user.can(auth));
-                                    } else if (auth instanceof Array) {
-                                        // should be an array of strings
-                                        // we sort the list first then we flatten
-                                        // the list into a string
-                                        // the string becomes the lookup key.
-                                        // this lets us define special auth lookups for
-                                        // each item individually and then when they
-                                        // are combined.
-                                        // if there is no rule for handling all items
-                                        // then each item is setup in a chain for sequential
-                                        // matching
-                                        auth.sort();
-                                        var combinedRoleName = auth.reduce(function(previousValue, currentValue, index, array) {
-                                            if (index > 0) {
-                                                previousValue += " ";
+                                            if (typeof auth == 'function') {
+                                                // auth is a function which means
+                                                // we don't have to do anything special
+                                                roles.use(fullRoleActionText, fullRoute, auth);
+                                                app[verb](fullRoute, user.can(fullRoleActionText));
+                                            } else if (typeof auth == 'string') {
+                                                // this means that we should use this string
+                                                // in place of the fullRoleActionText we
+                                                // are creating.
+                                                // the purpose of this is to allow reuse of
+                                                // auth code and to allow simple naming of
+                                                // requirements
+                                                //app[verb](fullRoute, user.can(auth));
+                                            } else if (auth instanceof Array) {
+                                                // should be an array of strings
+                                                // we sort the list first then we flatten
+                                                // the list into a string
+                                                // the string becomes the lookup key.
+                                                // this lets us define special auth lookups for
+                                                // each item individually and then when they
+                                                // are combined.
+                                                // if there is no rule for handling all items
+                                                // then each item is setup in a chain for sequential
+                                                // matching
+                                                auth.sort();
+                                                var combinedRoleName = flattenArray(auth);
+
+                                                if (true) {
+                                                    //console.log("USER.CAN(" + combinedRoleName + ");");
+                                                    app[verb](fullRoute, user.can(combinedRoleName));
+                                                } else {
+                                                    _.each(auth, function (individualAuthRole) {
+                                                        app[verb](fullRoute, user.can(individualAuthRole));
+                                                    });
+                                                }
                                             }
 
-                                            previousValue += currentValue;
 
-                                            return previousValue;
-                                        }, '');
-
-                                        if (true) {
-                                            app[verb](fullRoute, user.can(combinedRoleName));
-                                        } else {
-                                            _.each(auth, function(individualAuthRole) {
-                                                app[verb](fullRoute, user.can(individualAuthRole));
-                                            });
                                         }
+
+                                        //console.log(verb + ": " + fullRoute + " -> " + subRoute);
+                                        // setup actual route now
+                                        app[verb](fullRoute, subRoute);
                                     }
-
-
                                 }
-
-                                console.log(verb + ": " + fullRoute + " -> " + subRoute);
-                                // setup actual route now
-                                app[verb](fullRoute, subRoute);
                             }
-                        }
-                    });
+                        });
+                    }
                 }
             });
         }
     });
 
 };
+
+function flattenArray(stringArray) {
+    return stringArray.reduce(function (previousValue, currentValue, index) {
+        if (index > 0) {
+            previousValue += " ";
+        }
+
+        previousValue += currentValue;
+
+        return previousValue;
+    }, '');
+}
