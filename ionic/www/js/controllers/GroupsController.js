a3a3ee83c51d9de3cf816ca43b3da3f75a185692
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-08-11 20:58:49 -0500

    Update GroupsController to use BaseModelController

--- ionic/www/js/controllers/GroupsController.js
+++ ionic/www/js/controllers/GroupsController.js
@@ -4,25 +4,9 @@
 angular.module('scheduling-app.controllers')
     .controller('GroupsController', [
         '$scope',
-        'GroupsModel',
+        '$controller',
         'GENERAL_CONFIG',
-        function($scope, GroupsModel, GENERAL_CONFIG) {
-            $scope.groups = [];
-            $scope.add = function() {
-                $scope.groups.push({
-                    name: 'test!?',
-                    id: $scope.name.length
-                });
-            };
-            $scope.fetch = function() {
-                GroupsModel.getList().then(function(groups) {
-                    console.log("Successfully fetched groups!");
-                    $scope.groups = groups;
-                }, function(err) {
-                    $scope.groups = [
-                        {name: 'Failed to fetch groups'}
-                    ];
-                });
-            };
-            $scope.fetch();
+        function($scope, $controller, GENERAL_CONFIG) {
+            $controller('BaseModelController', {$scope: $scope});
+            $scope.register('GroupsModel', 'GroupsModel');
         }]);
