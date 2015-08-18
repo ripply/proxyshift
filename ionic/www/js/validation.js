b0de81f2e106d7152174755ed505ccb05c45ff76
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-08-18 08:42:17 -0500

    Add basic validators for Groups

--- ionic/www/js/validation.js
+++ ionic/www/js/validation.js
@@ -9,19 +9,26 @@
 // abstract enough that we can generate both client and
 // server side validations from it.
 
+var minimumStringLength = 4;
+var maximumStringLength = 20;
+
 var Validations = {
     User: {
         username: [
             { validator: 'notEmpty', message: 'Username is required.' },
-            { validator: 'isLength', args: [4, 20], message: 'Username must be between 4 and 20 characters.'},
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'Username must be between ' + minimumStringLength + ' and ' +
+                maximumStringLength + ' characters.'},
         ],
         firstname: [
             { validator: 'notEmpty', message: 'Fist name is required.'},
-            { validator: 'isLength', args: [0, 20], message: 'First name must be within 20 characters.'},
+            { validator: 'isLength', args: [1, maximumStringLength],
+                message: 'First name must be within ' + maximumStringLength + ' characters.'},
         ],
         lastname: [
             { validator: 'notEmpty', message: 'Last name is required.'},
-            { validator: 'isLength', args: [0, 20], message: 'Last name must be within 20 characters.'},
+            { validator: 'isLength', args: [1, maximumStringLength],
+                message: 'Last name must be within ' + maximumStringLength + ' characters.'},
         ],
         email: [
             { validator: 'notEmpty', message: 'Email is required.'},
@@ -30,7 +37,7 @@ var Validations = {
         password: [
             { validator: 'notEmpty', message: 'Password is required.'},
             // TODO: This works client side but server stores password hash which is larger than 20 chars so this needs to be changed
-            //{ validator: 'isLength', args: [4, 20], message: 'Password must be between 4 and 20 characters.'},
+            //{ validator: 'isLength', args: [minimumStringLength, 20], message: 'Password must be between ' + minimumStringLength + ' and 20 characters.'},
         ],
         squestion: [
             { validator: 'notEmpty', message: 'Secret question is required.'},
@@ -46,6 +53,54 @@ var Validations = {
         ],
         pagernumber: [
             { validator: 'isInt', message: 'Invalid Number.'},
+        ]
+    },
+    Group: {
+        name: [
+            { validator: 'notEmpty', message: 'Group must have a name' },
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'Group name must be within ' + minimumStringLength + ' and ' +
+            maximumStringLength},
+        ],
+        state: [
+            //{ validator: 'notEmpty', message: 'Group must have a state' },
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'Group state must be within ' + minimumStringLength + ' and ' +
+                maximumStringLength},
+        ],
+        city: [
+            //{ validator: 'notEmpty', message: 'Group must have a city' },
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'Group city must be within ' + minimumStringLength + ' and ' +
+                maximumStringLength},
+        ],
+        address: [
+            //{ validator: 'notEmpty', message: 'Group must have an address' },
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength * 4],
+                message: 'Group address must be within ' + minimumStringLength + ' and ' +
+                maximumStringLength * 4},
+        ],
+        zipcode: [
+            { validator: 'isInt', message: 'Invalid number'},
+        ],
+        weburl: [
+            //{ validator: 'notEmpty', message: 'Group must have a url' },
+            /*
+            { validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'URL must be within ' + minimumStringLength + ' and ' +
+                maximumStringLength},
+            //TODO: { validator: 'url' ...
+                */
+        ],
+        contactemail: [
+            { validator: 'notEmpty', message: 'Email must be present' },
+            /*{ validator: 'isLength', args: [minimumStringLength, maximumStringLength],
+                message: 'email must be within ' + minimumStringLength + ' and ' +
+                maximumStringLength}*/
+            //TODO: { validator: 'isEmail'}
+        ],
+        contactphone: [
+            { validator: 'isInt', message: 'Invalid phone number' }
         ]
     }
 
