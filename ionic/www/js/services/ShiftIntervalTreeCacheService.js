67069e83f9c375b2a2c2b74f7d54d07933cd68a3
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-09-17 18:29:44 -0500

    Small changes to interval tree cache

--- ionic/www/js/services/ShiftIntervalTreeCacheService.js
+++ ionic/www/js/services/ShiftIntervalTreeCacheService.js
@@ -18,10 +18,27 @@ angular.module('scheduling-app.controllers')
 
             this.add = function(shift) {
                 initialize();
-                intervalTree.insert(getInterval(shift));
+                intervalTree.insert(createInterval(shift));
             };
 
-            function getInterval(shift) {
+            this.remove = function(start, end) {
+                intervalTree.remove(start, end);
+            };
+
+            this.get = function(start, end) {
+                var shifts = [];
+
+                intervalTree.queryInterval(start, end, function(shiftInterval) {
+                    if (shiftInterval.length > 2) {
+                        shifts.push(shiftInterval[2]);
+                    }
+                    return false;
+                });
+
+                return shifts;
+            };
+
+            function createInterval(shift) {
                 // the library we use inserts entire array into tree
                 // so we can set the third array item
                 return [shift.start, shift.end, shift]
