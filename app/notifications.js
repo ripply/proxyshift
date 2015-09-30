f26904bd6416dbeb815cccd11b153c2268c6ce8f
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-09-29 23:14:47 -0500

    Adds very basic code to send push notification with message (to my phone)

--- app/notifications.js
+++ app/notifications.js
@@ -1,17 +1,44 @@
-var GCM = require('gcm').GCM,
-    config = require('config');
-
+var config = require('config');
 var apiKeys = {};
 
 if (config.has('push.gcm.serverapikey')) {
     apiKeys['gcm'] = config.get('push.gcm.serverapikey');
 }
 
-var gcm;
-if (apiKeys.gcm) {
-    gcm = new GCM(apiKeys.gcm);
+var gcm = require('node-gcm');
+
+function test() {
+    var message = new gcm.Message({
+        contentAvailable: true,
+        timeToLive: 3,
+        data: {
+            message: 'hey'
+        },
+        notification: {
+            title: 'title!',
+            body: 'body!'
+        }
+    });
+
+
+    var regIds = ['APA91bFySW60DcD29ZJSlGqPftJ0LKxr7GNZbqcT-9_2s4ju2JyQB0_a2D-G4aiHWaWedduQsH7fADooD_8yuOutqmHi6CCyaZzlFEKoObVBafiY9ugx_Ymmg4Z6lGquH3k7_AKag3rZ'];
+
+// Set up the sender with you API key
+    var sender = new gcm.Sender(apiKeys.gcm);
+
+// Now the sender can be used to send messages
+    sender.send(message, {registrationIds: regIds}, function (err, result) {
+        if (err) console.error(err);
+        else    console.log(result);
+    });
 }
-
+/*
+// Send to a topic, with no retry this time
+sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, result) {
+    if(err) console.error(err);
+    else    console.log(result);
+});
+*/
 module.exports = {
-
+    test: test
 };
