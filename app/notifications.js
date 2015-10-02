2eaabe9343bbf0fd020d098bc92d8627ca5b15f4
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-10-02 17:30:11 -0500

    Refactor notification code

--- app/notifications.js
+++ app/notifications.js
@@ -89,42 +89,15 @@ function _sendToGcm(message, endpoints) {
     })
 }
 
-Notifications.prototype.send = function(service, endpoint, alert, payload) {
-    if (false) {
-        console.log("Trying apn...");
-        var myDevice = new apn.Device("863974a9b8615f62a9af9c2a6f69a2e50bf9ceef8abd361bc84334e9c0e43eb7");
-        var note = new apn.Notification();
-        note.expiry = Math.floor(Date.now() / 1000) + 3600; // 1hr
-        note.badge = 3;
-        note.alert = "test";
-        note.payload = {'message': 'HEY!'};
-        apnConnection.pushNotification(note, myDevice);
+Notifications.prototype.send = function(service, endpoint, expires, message) {
+    var send = sendMap[service];
+    if (send) {
+        send(endpoints, expires, message);
+        return true;
     } else {
-        var message = new gcm.Message({
-            contentAvailable: true,
-            timeToLive: 3,
-            data: {
-                message: 'hey'
-            },
-            notification: {
-                title: 'title!',
-                body: 'body!'
-            }
-        });
-
-
-        var regIds = ['APA91bFySW60DcD29ZJSlGqPftJ0LKxr7GNZbqcT-9_2s4ju2JyQB0_a2D-G4aiHWaWedduQsH7fADooD_8yuOutqmHi6CCyaZzlFEKoObVBafiY9ugx_Ymmg4Z6lGquH3k7_AKag3rZ'];
-
-// Set up the sender with you API key
-        var sender = new gcm.Sender(apiKeys.gcm);
-
-// Now the sender can be used to send messages
-        sender.send(message, {registrationIds: regIds}, function (err, result) {
-            if (err) console.error(err);
-            else    console.log(result);
-        });
+        return null;
     }
-}
+};
 /*
 // Send to a topic, with no retry this time
 sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, result) {
@@ -132,7 +105,4 @@ sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, result) 
     else    console.log(result);
 });
 */
-module.exports = {
-    send: send,
-    platformMap: platformMap
-};
+module.exports = Notifications;
