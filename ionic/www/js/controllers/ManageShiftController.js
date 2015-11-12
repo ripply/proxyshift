a0594de4f457dc0a9c3d429c7288769a9eb00d90
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-11-12 07:48:09 -0600

    Show very basic list of shift applicants to manager

--- ionic/www/js/controllers/ManageShiftController.js
+++ ionic/www/js/controllers/ManageShiftController.js
@@ -1,23 +1,51 @@
 angular.module('scheduling-app.controllers')
     .controller('ManageShiftController', [
         '$scope',
+        '$rootScope',
         '$controller',
         '$stateParams',
+        'GENERAL_EVENTS',
         'GENERAL_CONFIG',
         'ManagingShiftsModel',
         function($scope,
+                 $rootScope,
                  $controller,
                  $stateParams,
+                 GENERAL_EVENTS,
                  GENERAL_CONFIG,
                  ManagingShiftsModel
         ) {
+            var RESOURCE = 'ManagingShifts';
             $controller('BaseModelController', {$scope: $scope});
             $scope.register(
-                'ManagingShifts',
+                RESOURCE,
                 ManagingShiftsModel,
                 undefined
             );
             $scope.shift_id = $stateParams.shift_id;
 
-            $scope.$on()
+            $rootScope.$on(GENERAL_EVENTS.UPDATES.RESOURCE, function(env, resource, newValue, oldValue) {
+                console.log("WAT");
+                getShiftId($scope.shift_id);
+            });
+
+            if ($rootScope[RESOURCE]) {
+                $scope._managingshifts = $rootScope[RESOURCE];
+                getShiftId($scope.shift_id);
+            }
+
+            function getShiftId(shift_id) {
+                shift_id = parseInt(shift_id);
+                if (!isNaN(shift_id) && $rootScope[RESOURCE]) {
+                    var shifts = $rootScope[RESOURCE];
+                    for (var i = 0; i < shifts.length; i++) {
+                        if (shifts[i].id === shift_id) {
+                            $scope.shift = shifts[i];
+                            return;
+                        }
+                    }
+                }
+
+                // TODO: Go back, invalid shift
+            }
         }]);
