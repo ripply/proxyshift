dc066ae33651283d8baef99afbb1d372e995df2e
Chase Ripplinger <chase.ripplinger@gmail.com>
2016-02-14 15:21:32 -0600

    Some work on styling shift description popup modal, pretty good looking will work on getting the description only to scroll now

--- ionic/www/js/controllers/popover/ShiftDescriptionController.js
+++ ionic/www/js/controllers/popover/ShiftDescriptionController.js
@@ -3,12 +3,14 @@ angular.module('scheduling-app.controllers')
         '$scope',
         '$rootScope',
         '$controller',
+        '$ionicScrollDelegate',
         'ShiftProcessingService',
         'GENERAL_CONFIG',
         'GENERAL_EVENTS',
         function($scope,
                  $rootScope,
                  $controller,
+                 $ionicScrollDelegate,
                  ShiftProcessingService,
                  GENERAL_CONFIG,
                  GENERAL_EVENTS
@@ -26,9 +28,35 @@ angular.module('scheduling-app.controllers')
                     $scope.show = true;
                     $scope.shift = shift;
                     $scope.waiting = true;
+                    //$ionicScrollDelegate.scrollTop(false);
+                    var shiftScrollContent = document.getElementById('shift-scroll-content');
+                    if (shiftScrollContent != null) {
+                        shiftScrollContent.setAttribute("style", "height: 0px");
+                    }
+
+                    var shiftDescriptions = document.getElementsByClassName('shift-description');
+                    angular.forEach(shiftDescriptions, function(shiftDescription) {
+                        shiftDescription.setAttribute('style', 'opacity: 0;');
+                    });
+
                     setTimeout(function() {
+                        var scrollContent = document.getElementById('shift-description-scroll-content');
+                        if (scrollContent) {
+                            var topPadding = 20;
+                            var buttonHeight = 47;
+                            var outerBottomPadding = 1;
+                            var outerTopPadding = 1;
+                            var maxHeight = (browserHeight() * 0.75) - buttonHeight - outerBottomPadding;
+                            var shiftScrollContent = document.getElementById('shift-scroll-content');
+                            var height = Math.min((scrollContent.clientHeight + topPadding), maxHeight) - outerBottomPadding - outerTopPadding;
+                            shiftScrollContent.setAttribute("style", "height: " + height + 'px');
+                            $ionicScrollDelegate.resize();
+                            angular.forEach(shiftDescriptions, function(shiftDescription) {
+                                shiftDescription.setAttribute('style', 'height: ' + (height + outerBottomPadding + buttonHeight + outerTopPadding ) + 'px');
+                            });
+                        }
                         $scope.waiting = false;
-                    }, 100);
+                    }, 0);
                 }
             });
 
@@ -43,6 +71,24 @@ angular.module('scheduling-app.controllers')
             $scope.userIsInDifferentTimeZone = ShiftProcessingService.userIsInDifferentTimeZone;
             $scope.getShiftsLocation = ShiftProcessingService.getShiftsLocation;
             $scope.getShiftsSublocation = ShiftProcessingService.getShiftsSublocation;
+
+            function browserHeight() {
+                var myWidth = 0, myHeight = 0;
+                if( typeof( window.innerWidth ) == 'number' ) {
+                    //Non-IE
+                    myWidth = window.innerWidth;
+                    myHeight = window.innerHeight;
+                } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+                    //IE 6+ in 'standards compliant mode'
+                    myWidth = document.documentElement.clientWidth;
+                    myHeight = document.documentElement.clientHeight;
+                } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+                    //IE 4 compatible
+                    myWidth = document.body.clientWidth;
+                    myHeight = document.body.clientHeight;
+                }
+                return myHeight;
+            }
         }
     ]
 );
