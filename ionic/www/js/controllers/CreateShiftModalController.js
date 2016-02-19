ec749dd5e2a04fbaee28aad51826905dff491872
Chase Ripplinger <chase.ripplinger@gmail.com>
2016-02-18 18:59:25 -0600

    Nav bar at bottom of new shift wizard

--- ionic/www/js/controllers/CreateShiftModalController.js
+++ ionic/www/js/controllers/CreateShiftModalController.js
@@ -13,10 +13,41 @@ angular.module('scheduling-app.controllers')
             $location
         ) {
             var calendarName = 'create-shift-calendar';
+
+            var steps = [
+                'create-shift-date',
+                'create-shift-when',
+                'create-shift-where',
+                'create-shift-who',
+                'create-shift-review'
+            ];
+
+            $scope.steps = {};
+            resetSteps();
+
+            function resetSteps() {
+                $scope.step = 0;
+                for (var i = 0, length = steps.length; i < length; i++) {
+                    var id = steps[i];
+                    $scope.steps[id] = {
+                        show: true,
+                        locked: i != 0
+                    };
+                }
+            }
+
+            function hideOtherSteps() {
+                for (var i = 0, length = steps.length; i < length; i++) {
+                    var id = steps[i];
+                    $scope.steps[id].show = i == $scope.step;
+                }
+            }
+
             $scope.$on('modal:createshift:reset', function() {
                 console.log('reset');
                 $ionicScrollDelegate.scrollTop(false);
                 $rootScope.$broadcast('events:calendar:reset', calendarName);
+                resetSteps();
             });
 
             $rootScope.$on('events:calendar:clicked', function(state, name, selected) {
@@ -26,7 +57,7 @@ angular.module('scheduling-app.controllers')
                     date.month(selected.month);
                     date.date(selected.day);
                     $scope.date = date;
-                    slideTo('create-shift-time');
+                    slideTo('create-shift-when');
                 }
             });
 
@@ -41,6 +72,13 @@ angular.module('scheduling-app.controllers')
             $scope.sliding = false;
 
             $scope.slideTo = slideTo;
+            $scope.navigateTo = navigateTo;
+
+            function navigateTo(location) {
+                if ($scope.steps.hasOwnProperty(location) && !$scope.steps[location].locked) {
+                    slideTo(location);
+                }
+            }
 
             function slideTo(location) {
                 if ($scope.sliding) {
@@ -51,13 +89,24 @@ angular.module('scheduling-app.controllers')
                 blockInput(true);
                 $location.hash(location);
                 $ionicScrollDelegate.anchorScroll(true);
+                if ($scope.steps.hasOwnProperty(location)) {
+                    $scope.step = steps.indexOf(location);
+                    $scope.steps[location].locked = false;
+                } else {
+                    // unknown, show first
+                    $scope.step = 0;
+                    $scope.steps[steps[0]].show = true;
+                }
+                console.log(location);
+                console.log($scope.step);
+                hideOtherSteps();
                 $timeout(function() {
                     blockInput(false);
                     $location.hash(location);
                     $ionicScrollDelegate.anchorScroll(false);
                     $scope.sliding = false;
                 }, 500);
-            };
+            }
 
             function blockInput(block) {
                 $scope.blockInput = block;
