b939e676a21bfa3690dc364c1236798e3e9fc1de
Chase Ripplinger <chase.ripplinger@gmail.com>
2016-02-23 22:29:21 -0600

    Su => Sun

--- ionic/www/js/controllers/ShiftCalendarController.js
+++ ionic/www/js/controllers/ShiftCalendarController.js
@@ -168,7 +168,7 @@ angular.module('scheduling-app.controllers')
 
                 var weekData = [];
                 for (var i = 0; i < 7; i++) {
-                    weekData.push(startOfWeek.format("dd"));
+                    weekData.push(startOfWeek.format("ddd"));
                     startOfWeek.add(1, "day");
                 }
 
