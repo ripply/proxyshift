faaf7bb594140e595d1e2d23638e1057f49482b1
Chase Ripplinger <chase.ripplinger@gmail.com>
2016-06-18 14:53:17 -0500

    Revert "HACK TO SORT /ALL, LAST HACK WAS FOR /MANAGING"
    This reverts commit 4ec647f7dd09c8e6307e5badd146b7910e8781ff.

--- controllers/shifts.js
+++ controllers/shifts.js
@@ -204,25 +204,7 @@ module.exports = {
                     .then(function(shifts) {
                         if (shifts) {
                             // TODO: Fetch related group user class information
-                            var shiftJson = shifts.toJSON();
-                            // TODO: GET RID OF THIS SORT AND OPTIMIZE QUERY TO GET ORDERBY TO BE RESPECTED WITH UNIONS
-                            // https://stackoverflow.com/questions/213851/sql-query-using-order-by-in-union
-                            shiftJson.sort(function(left, right) {
-                                if (left.start == right.start) {
-                                    if (left.end == right.end) {
-                                        return 0;
-                                    } else if (left.end < right.end) {
-                                        return -1;
-                                    } else {
-                                        return 1;
-                                    }
-                                } else if (left.start < right.start) {
-                                    return -1;
-                                } else {
-                                    return 1;
-                                }
-                            });
-                            res.json(shiftJson);
+                            res.json(shifts.toJSON());
                         } else {
                             res.json([]);
                         }
